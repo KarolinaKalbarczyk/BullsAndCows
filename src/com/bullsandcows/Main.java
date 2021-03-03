311089c7991014d0c0ccaad86ff644c617d9828c
KarolinaKalbarczyk <kalbarczyk.karolina@gmail.com>
2021-03-03 21:41:03 +0100

    Stage 4

--- src/com/bullsandcows/Main.java
+++ src/com/bullsandcows/Main.java
@@ -3,12 +3,19 @@ package com.bullsandcows;
 import java.util.Scanner;
 import java.util.*;
 
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("How many digits should the random number contain?");
+        System.out.println("Please, enter the secret code's length:");
+
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
+
+        if(n > 10 || n < 0){
+            System.out.println("Error: can't generate a secret number with a length of " + n + " because there aren't enough unique digits.");
+            System.exit(0);
+        }
 
         long pseudoRandomNumber = System.nanoTime();
 
@@ -32,12 +39,46 @@ public class Main {
         }
 
         String delimiter = "";
+        String result = String.join(delimiter, linkedHashSetToArray).substring(0,n);
 
-        if(n < 10) {
-            System.out.println("The random secret number is " + String.join(delimiter, linkedHashSetToArray).substring(0,n) + ".");
-        } else {
-            System.out.println("Error: can't generate a secret number with a length of 11 because there aren't enough unique digits.");
+        System.out.println("Okay, let's start a game!");
+
+        int bulls = 0;
+        int turn = 1;
+
+        while(bulls != n){
+
+            int cows = 0;
+            bulls = 0;
+            System.out.println("Turn " + turn + " :");
+            Scanner scanner = new Scanner(System.in);
+
+            //the user gives his number 
+            String fromUser = scanner.nextLine();
+
+            for(int i = 0; i < n; i++){
+                if(fromUser.charAt(i) == result.charAt(i)){
+                    bulls++;
+                }
+                for (int j = i+1; j < n; j++){
+                    if(fromUser.charAt(i) == result.charAt(j)){
+                        cows++;
+                    }
+                }
+            }
+
+            if(cows > 0 && bulls > 0){
+                System.out.println("Grade: " + bulls + " bull(s) and " + cows + " cow(s).");
+            } else if (cows > 0 && bulls == 0){
+                System.out.println("Grade: " + cows + " cow(s).");
+            } else if (cows == 0 && bulls > 0){
+                System.out.println("Grade: " + bulls + " bull(s).");
+            } else if (bulls == 0 && cows == 0){
+                System.out.println("Grade: None.");
+            }
+
+            turn++;
         }
-
+        System.out.println("Congratulations! You guessed the secret code.");
     }
 }
